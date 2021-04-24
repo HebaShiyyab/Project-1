6fd2e311dc9e2f9491d9f003df3181f01ce8fde5
HebaShiyyab <alshyabheba@gmail.com>
2021-04-25 00:40:46 +0300

    project-1

--- project.js
+++ project.js
@@ -1,9 +1,30 @@
-class Game(x , o){
-this.playerA = x;
-this.playerB = o;
-this.winner(){
+class Game(X , O){
+    let items = new Array(9).fill(undefined);
+    let result = undefined;
+    let turn = 'X';
 
-}
-if (x    )
+    const winCombinations=[
+    
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+          ];
+    function setValue(i){
+        items[i]=turn;
+        items=[...items];
+        turn= turn=='X'?'O':'X';
+        if(!items.includes(undefined)){
+
+        }else{
+           checkWinner(); 
+        }
+
+
+
 
 }
