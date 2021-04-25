89d29274c73b35e04eb529212f34d7491bd6a160
HebaShiyyab <alshyabheba@gmail.com>
2021-04-26 01:37:45 +0300

    project-1

--- project.js
+++ project.js
@@ -2,23 +2,24 @@ document.addEventListener('click',()=>{
 const parent = document.querySelectorAll('.child div')
 const playerDisplay = document.querySelector('#player')
 let currentPlayer = 'playerX'
+const itemArray = Array.from(parent)
+
 parent.forEach(item=> {
     item.addEventListener('click', clickOutCome)
 
 })
 function clickOutCome(i){
-    const itemArray = Array.from(parent)
-    const items = itemArray.indexOf(i.target)
+    const index = itemArray.indexOf(i.target)
     playerDisplay.innerHTML = currentPlayer
-}
-if(currentPlayer=== 'playerX' ){
+
+if(currentPlayer === 'playerX' ){
     currentPlayer = 'playerO'
     parent[index].classList.add('playerX')
 }else{
     currentPlayer = 'playerX'
      parent[index].classList.add('playerO')
 }
-})
+}})
 
 
 
